0ef5787fdd961fc782cec5a996435c7616d78305
tayllan <tayllanburigo@gmail.com>
2015-05-31 16:35:59 -0300

    Altered database user and password

--- site/models/DAO.py
+++ site/models/DAO.py
@@ -2,7 +2,7 @@ import psycopg2
 
 class DAO:
 
-	connection_string = "host='localhost' dbname='tales' user='postgres' password='supremeTolkien'"
+	connection_string = "host='localhost' dbname='tales' user='anaddventure' password='dozeDolares'"
 	connection = psycopg2.connect(connection_string)
 
 	@staticmethod
